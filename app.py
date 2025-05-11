769396e683fb15f9de6925e391802607f88affec
Edi <pythontestniracun@gmail.com>
2025-05-11 16:44:14 +0200

    save point sve radi nakon updejta slanja maila preko kontakt forme

--- app.py
+++ app.py
@@ -1,24 +1,38 @@
-from flask import Flask
-from flask import render_template, redirect, request, url_for
-
+from flask import Flask, render_template, flash, redirect, url_for, request
+from flask_mail import Mail, Message
+from forms import ContactForm
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
+
+load_dotenv()
+# Mail config
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')  # Get email from .env
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')  # Get password from .env
+mail = Mail(app)
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route('/contact', methods=['GET', 'POST'])
-def contact():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        message = request.form['message']
-        # Process form data here (e.g., save to database or send email)
-        return redirect(url_for('index'))
-    return render_template('contact.html')
 
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    form = ContactForm()
+    if form.validate_on_submit():
+        msg = Message(subject=f"Kontakt forma od {form.name.data}",
+                      sender=app.config['MAIL_USERNAME'],
+                      recipients=['pythontestniracun@gmail.com'],
+                      body=f"Ime: {form.name.data}\nEmail: {form.email.data}\nPoruka:\n{form.message.data}")
+        mail.send(msg)
+        flash("Poruka je uspješno poslana!")
+        return redirect(url_for("contact"))
+    return render_template("contact.html", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
